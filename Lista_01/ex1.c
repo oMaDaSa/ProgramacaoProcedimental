b9e19622a5e9e76554825914656cafc9d21df988
Matheus Dantas <65237455+oMaDaSa@users.noreply.github.com>
2023-03-21 13:11:05 -0300

    Delete Lista_01 directory

--- Lista_01/ex1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-//Faça um programa que leia um número inteiro e o imprima.
-int main()
-{
-    int x;
-    scanf("%d", &x);
-    printf("Numero digitado: %d", x);
-    return 0;
-}
--- Lista_01/ex2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-//Faça um programa que leia um número real e o imprima.
-int main()
-{
-    float x;
-    scanf("%f", &x);
-    printf("Numero digitado: %f", x);
-    return 0;
-}
